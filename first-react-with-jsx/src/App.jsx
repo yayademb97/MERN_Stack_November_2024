ab25f8799de931594aa5442b1889eef7c7f27f57
Yayanefel10 <yaya.dembele@nefel.education>
2024-12-12 17:32:15 +0100

    First React With JSX

---
+++ first-react-with-jsx/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hello Dojo!</h1>
+     <h4>Things I need to do</h4>
+     <ul>
+      <li>Learn React</li>
+      <li>Climb Mt Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feeds the dogs</li>
+     </ul>
+    </>
+  )
+}
+
+export default App
